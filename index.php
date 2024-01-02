43633ddcaf73ea8eb58d8aed6a981495b0beab8d
Anna Argylova <33651891+ArRyabkina@users.noreply.github.com>
2024-01-03 00:26:15 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>hello</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="bg-info razmer">
+		<div class="p-2">
+		<div class="row mt-5">
+			<div class="col-4 ml-auto bord p-2">
+				<h4 class="text-right"> Р е ч н о й п о р т</h4>
+			</div>
+			<div class="col-4 mr-auto bord1 p-2">
+				<h4 class="text-left mt-3">Я к у т и я</h4>
+			</div>
+		</div>
+		</div>
+		<div class="mx-5 mt-5">
+			<h3 class="text-center">
+				Приветствуем вас в официальном приложении Якутского Речного порта
+			</h3>
+			<p class="text-center mt-3 mb-3">
+				Для использования всех функций нужно зарегистрироваться.
+			</p>
+			<div class="row">
+				<div class="col-sm">
+					<form class="text-right">
+						<button  type="button" class="btn btn-info">Регистрация</button>
+					</form>	
+				</div>
+				<div class="col-sm">
+					<form class="text-left">
+						<button type="button" class="btn btn-light">Вход</button>
+					</form>	
+				</div>
+			</div>
+		</div>
+		
+	</div>
+</body>
+</html>
